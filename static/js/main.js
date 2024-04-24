8c9fa9a58f245c37119a2a5979ec478d2c70ebab
maundulaurent <charlesmaundu16@gmail.com>
2024-04-24 11:46:58 +0300

    Inserted a consistent Navbar across the web pages

--- static/js/main.js
+++ static/js/main.js
@@ -15,19 +15,7 @@
     // Initiate the wowjs
     new WOW().init();
     
-    
-   // Back to top button
-   $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-        $('.back-to-top').fadeIn('slow');
-    } else {
-        $('.back-to-top').fadeOut('slow');
-    }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+
 
 
     // Modal Video
@@ -109,4 +97,3 @@
 
 
 })(jQuery);
-
