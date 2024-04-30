62d9420793db25205e5ea4b83a51f740101b301d
Giulio <mukaathegiulio@gmail.com>
2024-04-30 18:26:09 +0300

    conflicting chages resolved

--- Buniwa/models.py
+++ Buniwa/models.py
@@ -1,16 +1,13 @@
 from django.db import models
 
-<<<<<<< HEAD
 class theBlog(models.Model):
     title = models.CharField(max_length=20)
     category = models.CharField(max_length=100)
     # content = models.TextField()
-=======
 class Card(models.Model):
     title = models.CharField(max_length=50)
     category = models.CharField(max_length=100)
     content = models.TextField()
->>>>>>> 2a242e0ccc7c1ec90306a6f34bff775db59f3b49
     author = models.CharField(max_length=100)
     published_date = models.DateField()
     card_image = models.ImageField(upload_to='card_images/')
@@ -18,12 +15,9 @@ class Card(models.Model):
 
     def __str__(self):
         return self.title
-<<<<<<< HEAD
     
     class Meta:
         verbose_name_plural = 'Blogs Section'
-=======
->>>>>>> 2a242e0ccc7c1ec90306a6f34bff775db59f3b49
 
 
         
@@ -35,13 +29,9 @@ class PortfolioPost(models.Model):
     def __str__(self):
         return self.title
     
-<<<<<<< HEAD
-    class Meta:
-        verbose_name_plural = 'Portfolio Section'
+class Meta:
+    verbose_name_plural = 'Portfolio Section'
     
-=======
->>>>>>> 2a242e0ccc7c1ec90306a6f34bff775db59f3b49
-
 class theTeam(models.Model):
     team_name = models.CharField(max_length=20)
     team_position = models.CharField(max_length=50)
@@ -54,10 +44,6 @@ class theTeam(models.Model):
     def __str__(self):
         return self.team_name
     
-<<<<<<< HEAD
     class Meta:
         verbose_name_plural = 'Teams Section'
     
-=======
->>>>>>> 2a242e0ccc7c1ec90306a6f34bff775db59f3b49
-
